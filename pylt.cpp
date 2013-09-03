b5b55803c3cafb1640383f36ae3818b54153d2d1
Alexander Kulpanov <post@kulpanov.ru>
2013-09-03 17:25:29 +0400

    Соображения по коду

--- pylt.cpp
+++ pylt.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "pylt.h"
 #include "ui_pylt.h"
 
@@ -41,6 +43,11 @@ Pylt::~Pylt()
 
 void Pylt::AllGo()
 {
+    //FIXME: заметь здесь можно сделать цикл, как то так:
+    //for(int i=0;i<suppButtons.size();i++) suppButtons[i]->setChecked(1);
+    // конечно в конструкторе этот массив надо проиниц, для массива всегда лучше пользовать std::vector
+
+
 ui->pushButton_4->setChecked(1);
 ui->pushButton_5->setChecked(1);
 ui->pushButton_6->setChecked(1);
@@ -72,28 +79,36 @@ void Pylt::Stop()
      ui->pushButton_15->setChecked(0);
 
 }
+
+
 void Pylt::Plus()
 {
+    std::cout << "Plus" << std::endl;
+    //TODO:
+    //Логика следующая:
+    //необходимо знать виджет(LineEdit) в каком будем изменять данные
+    //Это виджет по которому последний раз тыкнули мышой, либо кто то по умолчанию (если еще никуда не тыкали)
+    //Вводим поле currentLineEdit, в конструкторе присваеваем ui->lineEdit - это будет по умолчанию.
+    //Для обработки кого последний раз ткнули мышой:
+    //на lineEdit'ы вешаем обработчик на сигнал приобретения фокуса (как то так должен называться)
+    //в этом обработчике пишем как то так:
+    //currentLineEdit = sender;  - те кто прислал сигнал тот и последний
 
-
-
-
+    //а в методе Plus  делаем как то так:
+    //QString lValue = currentLineEdit->text();
+    //lValue = QString().fromInt(lValue.toInt() ++);
+    //currentLineEdit->setText(lValue);
 
 
 }
 
 void Pylt::Minus()
-
 {
 
-
-
-
+    std::cout << "Minus" << std::endl;
 }
 
 void Pylt::Period()
 {
-
-
-
+    std::cout << "Period" << std::endl;
 }
